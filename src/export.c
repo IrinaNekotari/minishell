c5a304f1f6f2f48eae03836c5813ead5a124033e
MartinJuette <138514075+MartinJuette@users.noreply.github.com>
2023-11-14 18:54:12 +0100

    export a moitié copié qui marche pas

---
+++ src/export.c
@@ -0,0 +1,105 @@
+#include "minishell.h"
+
+static int  print_error(int error, const char *arg)
+{
+    int i;
+
+    if(error ==-1)
+        ft_putstr_fd("export: not valid in this context: ", STDERR);
+    else if (error == 0 || error == -3)
+        ft_putstr_fd("export: not a valid identifier: ", STDERR);
+    i = 0;
+    while (arg[i] && (arg[i] != '=' || error == -3))
+    {
+        write(STDERR, &arg[i], 1);
+        i++;
+    }
+    write(STDERR, "\n", 1);
+    return (ERROR);
+}
+
+int env_add(const char *value, t_env *env)
+{
+    t_env*new;
+    t_env*tmp;
+
+    if (env && env->val == NULL)
+    {
+        env->val = ft_strdup(value);
+        return (SUCCESS);
+    }
+    if (!(new = malloc(sizeof(t_env))))
+        return (-1);
+    new->val = ft_strdup(value);
+    while (env && env->next && env->next->next)
+        env = env->next;
+    tmp = env->next;
+    env->next = new;
+    new->next = tmp;
+    return (SUCCESS);
+}
+
+char *get_name(char *dest, const char *src)
+{
+    int i;
+
+    i = 0;
+    while (src[i] && src[i] != '=' && ft_strlen(src) < BUFF_SIZE)
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return (dest);
+}
+
+int is_in_env(t_env *env, char *args)
+{
+    char var_name[BUFF_SIZE];
+    char env_name[BUFF_SIZE];
+
+    get_env_name(var_name, args);
+    while (env && env->next)
+    {
+        get_env_name(env_name, env->val);
+        if (ft_strcmp(var_name, env_name) == 0)
+        {
+            ft_memdel(env->val);
+            env->val = ft_strdup(args);
+            return (1);   
+        }
+        env = env->next;
+    }
+    return (SUCCESS);
+ }
+
+int ft_export(char **args, t_env *env, t_env *secret)
+{
+    int new_env;
+    int error_ret;
+
+    new_env = 0;
+    if(!args[1])
+    {
+        print_sorted_env(secret);
+        return (SUCCESS);
+    }
+    else
+    {
+        error_ret = is_valid_env(args[1]);
+        if (args[1][0] == '=')
+            error_ret = -3;
+        if(error_ret <= 0)
+            return (print_error(error_ret, args[1]));
+        new_env = error_ret == 2 ? 1 : is_in_env(env, args[1]);
+        if (new_env == 0)
+        {
+            if (error_ret == 1)
+                env_add(args[1], env);
+            env_add(args[1], secret);
+        }
+    }
+    return (SUCESS);
+}
+
+}
